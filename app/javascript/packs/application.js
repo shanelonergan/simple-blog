bb956e08b4ebea3e923cc014622e03e4d591e846
github username <github email address>
2019-08-30 15:20:15 -0400

    added bootstrap functionality, fixed index"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,5 +16,6 @@ require("bootstrap")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-= require bootstrap
-//= require jquery
+//= require bootstrap
+//= require popper
+//= require jquery3
